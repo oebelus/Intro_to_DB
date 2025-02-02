f038740e6c249acc4e5dbaeeeed6a0486f18f015
oebelus <imaneelfidrissi@hotmail.com>
2025-02-02 18:40:52 +0100

    IF NOT EXISTS

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT FOREIGN KEY REFERENCES Authors(author_id),
@@ -9,25 +9,25 @@ CREATE TABLE Books (
     publication_date DATE
 );
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE Customers (
+CREATE IF NOT EXISTS TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
     order_date DATE
 );
 
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT FOREIGN KEY REFERENCES Orders(order_id),
     book_id INT FOREIGN KEY REFERENCES Books(book_id),
