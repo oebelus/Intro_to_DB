23cab934d996aca4458d96db1a2dc38fe6aeb452
oebelus <imaneelfidrissi@hotmail.com>
2025-02-02 18:49:50 +0100

    adding exceptions to the mysql server

--- MySQLServer.py
+++ MySQLServer.py
@@ -10,9 +10,12 @@ mydb = mysql.connector.connect(
 if mydb.is_connected():
     cursor = mydb.cursor()
 
-    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    print("Database 'alx_book_store' created successfully or already exists.")
-    
+    try:
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully or already exists.")
+    except mysql.connector.Error as err:
+        print(f"Error creating database: {err}")
+        
     cursor.close()
-    
+
 mydb.close()
